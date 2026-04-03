fe7772f42c9b690637cc50b40319749edf62cab9
minyoungk1539 <minyoung.k1539@gmail.com>
2026-04-04 01:39:48 +0900

    Update 백준 1158.py

--- 2nd/백준 1158.py
+++ 2nd/백준 1158.py
@@ -1,16 +1,16 @@
 import collections
 
-N, K = map(int, input().split())
-q = collections.deque([])
-p = collections.deque([])    
+N, K = map(int, input().split())  #N명의 사람, K씩 띄우면서 제거
+q = collections.deque([])  #기존 사람들
+p = collections.deque([])   #제거된 사람들
 for i in range(1, N+1) : 
     q.append(i)  #q에 1~N 삽입
 
 
 for l in range (1, N+1):
-    q.rotate(-K)
-    a = q.pop()
-    p.append(a)
+    q.rotate(-K)  #K씩 반시계방향 회전
+    a = q.pop()  #해당 사람 제거
+    p.append(a)  #새로운 덱인 p에 삽입
 
 result = "<" + ", ".join(map(str, p)) + ">"
 print(result)
